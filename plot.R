1d3f1df224d552741cd385e92cd6f74a0d530386
dnegrey <dnegrey@gmail.com>
2016-12-16 16:07:51 -0500

    added h

--- plot.R
+++ plot.R
@@ -1,9 +1,15 @@
 library(ggplot2)
 pts <- data.frame(
     x = c(10:20,
-          rep(15, 15)),
+          rep(15, 15),
+          rep(25, 16),
+          25:30,
+          rep(31, 9)),
     y = c(rep(80, 11),
-          79:65)
+          79:65,
+          80:65,
+          rep(73, 6),
+          73:65)
 )
 ggplot(data = pts, aes(x, y)) + 
     geom_point() + 
